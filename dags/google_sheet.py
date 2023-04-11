e245373f6a77409895f7ba2b14cd60489218ff77
Cody <huynhdangthinh@gmail.com>
2023-04-11 20:34:41 +0700

    update full workflow rawl data from google sheet & email

--- dags/google_sheet.py
+++ dags/google_sheet.py
@@ -1,13 +1,7 @@
 from __future__ import print_function
 
-import os.path
-
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from google.cloud import bigquery
 from google.oauth2 import service_account
 
 import pandas as pd
@@ -17,127 +11,106 @@ import psycopg2
 
 import os
 import json
-
-
-# If modifying these scopes, delete the file token.json.
-#SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-SCOPES = ['https://www.googleapis.com/auth/cloud-platform']
+import numpy as np
 
 # The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1lhMOvIhZMlcUhmlD1murK_m3AepzREZz8cJR5P7ldd4'
-SAMPLE_RANGE_NAME = 'Task!A1:L'
+SPREADSHEET_ID = '1qKy8Q84RbJZo7_QHRIudXkgqgLolzbNvgFheUf06wbg'
 key_path = '/opt/airflow/data/credentials_service.json'
 service_account_info = json.load(open(key_path))
 creds = service_account.Credentials.from_service_account_info(service_account_info)
 
-def gg_conn():
+#engine = create_engine("postgresql://kitpdzdt:123456@tiny.db.elephantsql.com:5432/kitpdzdt")
+engine = create_engine("postgresql://postgres:123456@host.docker.internal") 
+
+def google_sheet_conn():
+    pass
+
+def send_mail():
+    pass
+
+def gg_conn(rang_name, col_num):
     try:
         service = build('sheets', 'v4', credentials=creds)
-
+        
         # Call the Sheets API
         sheet = service.spreadsheets()
-        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                    range=SAMPLE_RANGE_NAME).execute()
+        result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=rang_name).execute()
         values = result.get('values', [])
        
         if not values:
             print('No data found.')
             return
-
-        # Add data to dataframe
-        dic = {}
-
-        for i in range(1, 12):
-            lst = []
-            for row in values:
-                lst.append(row[i])
-                dic[lst[0]] = lst[1:]
         
     except HttpError as err:
         print(err)
-    return dic
+    return values
 
-def main():
-    """Shows basic usage of the Sheets API.
-    Prints values from a sample spreadsheet.
-    """
-    creds = None
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    #If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                '/opt/airflow/data/credentials.json', SCOPES)
-            #creds = flow.run_local_server(port=0)
-            creds = flow.run_local_server(
-                    host='localhost',
-                    port=8088,
-                    authorization_prompt_message='Please visit this URL: {url}',
-                    success_message='The auth flow is complete; you may close this window.',
-                    open_browser=True)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
+def insert_master():
     try:
-        service = build('sheets', 'v4', credentials=creds)
+        lst = gg_conn('Master Data!A1:H', 8)
+        dic_master = pd.DataFrame(lst[1:], columns=lst[0])
+        df_master = pd.DataFrame.from_dict(dic_master)
+        df_master = df_master.replace(to_replace=[''], value=np.nan)
+        df_master = df_master.dropna(subset=["Key"])
+        connection = engine.connect()
+        metadata = MetaData()
 
-        # Call the Sheets API
-        sheet = service.spreadsheets()
-        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                    range=SAMPLE_RANGE_NAME).execute()
-        values = result.get('values', [])
-       
+        tb = Table("tb_master", metadata, autoload=True, autoload_with=engine)
 
-        if not values:
-            print('No data found.')
-            return
-
-        # Add data to dataframe
-        dic = {}
-
-        for i in range(1, 12):
-            lst = []
-            for row in values:
-                lst.append(row[i])
-                dic[lst[0]] = lst[1:]
-        
+        for index, row in df_master.iterrows():
+            stmt = insert(tb).values(ticket_id=row["Key"]
+                                    , assignee=row["Assignee"]
+                                    , issue_type=row["Issue Type"]
+                                    , summary=row["Summary"]
+                                    , status=row["Status"]
+                                    , sprint=["Sprint"]
+                                    , est_story_point=row["Est. Story Points"]
+                                    , delayed_task_flag=row["Delayed Task Flag"]
+                                    )
+            result_proxy = connection.execute(stmt)
     except HttpError as err:
         print(err)
-    return dic
 
-# if __name__ == '__main__':
-#     dic = main()
+def insert_sprint():
+    try:
+        lst = gg_conn('Sprint Data!A1:D', 4)
+        dic_sprint=pd.DataFrame(lst[1:], columns=lst[0])
+        df_sprint = pd.DataFrame.from_dict(dic_sprint)
+        df_sprint = df_sprint.replace(to_replace=[None, 'None', '|'], value=np.nan)
+        #df_sprint = df_sprint.dropna(subset=['S'])
+        df_sprint.rename(columns={'S':'Key'}, inplace=True)
 
-# # Turn dict into DataFrame
-# df = pd.DataFrame.from_dict(dic)
-# print(df.head())
+        connection = engine.connect()
+        metadata = MetaData()
 
+        tb = Table("tb_sprint", metadata, autoload=True, autoload_with=engine)
 
-# # Load into Postgre
-# engine = create_engine("postgresql://postgres:123456@localhost") 
-# #engine = create_engine("postgresql://fdobijxb:P4e-KqTZVBR6FhiME3GflCNzn4vIPVng@tiny.db.elephantsql.com:5432/fdobijxb")
-# #engine = create_engine("postgresql://kitpdzdt:123456@tiny.db.elephantsql.com:5432/kitpdzdt")
+        for index, row in df_sprint.iterrows():
+            stmt = insert(tb).values(ticket_id=row["Key"]
+                                    , sprint=row["Sprint"]
+                                    , pi=row["PI"]
+                                    , iteration=row["Iteration"]
+                                    )
+            result_proxy = connection.execute(stmt)
+    except HttpError as err:
+        print(err)
 
+def insert_issue():
+    try:
+        lst = gg_conn('Issue Data!A1:B', 2)
+        dic_issue=pd.DataFrame(lst[1:], columns=lst[0])
+        df_issue = pd.DataFrame.from_dict(dic_issue)
+        df_issue.rename(columns={"Log Work.comment":"Issue"}, inplace=True)
 
-# connection = engine.connect()
-# metadata = MetaData()
+        connection = engine.connect()
+        metadata = MetaData()
 
-# columns = ["job_type", "status"]
-# tb = Table("tb_type", metadata, autoload=True, autoload_with=engine)
-# #lst = df.to_dict("records")
-# # stmt = insert(tb)
-# # result_proxy = connection.execute(stmt, lst=columns)
+        tb = Table("tb_issue", metadata, autoload=True, autoload_with=engine)
 
-# for index, row in df.iterrows():
-#     stmt = insert(tb).values(job_type=row["Job Type"], status=row["Status"])
-#     result_proxy = connection.execute(stmt)
-
-#     #tmt = "INSERT INTO job_type (job_type) VALUES (%s)"
-#     #val = row["Job Type"]
-
-#     #result_proxy = connection.execute(stmt, val)
-
+        for index, row in df_issue.iterrows():
+            stmt = insert(tb).values(ticket_id=row["Key"]
+                                    , issue=row["Issue"]
+                                    )
+            result_proxy = connection.execute(stmt)
+    except HttpError as err:
+        print(err)
